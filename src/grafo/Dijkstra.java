1e382dcc325aabe924cad13e3c0de695451ad25c
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2023-11-05 08:50:43 -0300

    Ajustes Classes/Correcao fila

---
+++ src/grafo/Dijkstra.java
@@ -0,0 +1,146 @@
+package grafo;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+
+    private ArrayList<Vertice> listaAdj = new ArrayList<>();
+
+    public Dijkstra(ArrayList<Vertice> listaAdj) {
+        this.listaAdj = listaAdj;
+    }
+
+    public ArrayList<Vertice> getListaAdj() {
+        return listaAdj;
+    }
+
+    public void setListaAdj(ArrayList<Vertice> listaAdj) {
+        this.listaAdj = listaAdj;
+    }
+
+    public String[][] calcularRota(String id){
+
+        return null;
+
+    }
+
+    public Vertice getVertice(String id) {
+        
+        for (Vertice v : listaAdj) {
+            if (v.getId().equals(id)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    private String[][] dijkstra(String id) {
+        Vertice v = this.getVertice(id);
+        if (v != null) {
+            this.initializaSingleSource(id);
+        } else {
+            return null;
+        }
+
+        ArrayList<Vertice> s = new ArrayList<>();
+        PriorityQueue<Vertice> q = new PriorityQueue<>(new VerticeDijkstraComparator());
+        for (Vertice vertice : listaAdj) {
+            q.add(vertice);
+        }
+
+        while (!q.isEmpty()) {
+            Vertice u = q.poll();
+            s.add(u);
+            ArrayList<Aresta> listaArestas = u.getListaArestas();
+            PriorityQueue<Aresta> filaArestas = new PriorityQueue<>(new ArestaComparator());
+            for (Aresta aresta : listaArestas) {
+                filaArestas.add(aresta);
+            }
+            while (!filaArestas.isEmpty()) {
+                Aresta aresta = filaArestas.poll();
+                Vertice adj = aresta.getVerticeAdj();
+                
+                this.relax(u, adj, aresta.getCusto(), q);
+                
+            }
+
+        }
+        return null;
+
+    }
+
+    private void initializaSingleSource(String id) {
+
+        for (Vertice v : listaAdj) {
+            v.setD(Integer.MAX_VALUE);
+            v.setPi("nill");
+
+            if (v.getId().equals(id)) {
+                v.setD(0);
+                v.setPi("nill");
+            }
+        }
+
+    }
+
+    private void relax(Vertice u, Vertice verticeAdj, int custo, PriorityQueue<Vertice> q) {
+
+        if (verticeAdj.getD() > u.getD() + custo) {
+            q.remove(verticeAdj);
+            verticeAdj.setD(u.getD() + custo);
+            verticeAdj.setPi(u.getId());
+            q.add(verticeAdj);
+        }
+    }
+
+    public String vetorRoteamentoToString(String id){
+        this.dijkstra(id);
+        StringBuilder str = new StringBuilder();
+        int size = listaAdj.size();
+        String[][] st = new String[listaAdj.size()][5];
+
+        for (int i = 0; i < listaAdj.size(); i++) {
+            Vertice v = listaAdj.get(i);
+            st[i][0] = v.getId();
+            st[i][1] = Integer.toString(v.getD());
+            st[i][2] = v.getPi();
+            st[i][3] = " ";
+            st[i][4] = "x";
+        }
+
+        // Cabeçalhos
+        str.append(String.format("%-8s", "VERTICE"));
+        for (int i = 0; i < size; i++) {
+            str.append(String.format("%-8s", st[i][0]));
+        }
+        str.append("\n");
+
+        str.append(String.format("%-8s", "d"));
+        for (int i = 0; i < size; i++) {
+            str.append(String.format("%-8s", st[i][1]));
+        }
+        str.append("\n");
+
+        str.append(String.format("%-8s", "Pi"));
+        for (int i = 0; i < size; i++) {
+            str.append(String.format("%-8s", st[i][2]));
+        }
+        str.append("\n");
+
+        str.append(String.format("%-8s", "q"));
+        for (int i = 0; i < size; i++) {
+            str.append(String.format("%-8s", st[i][3]));
+        }
+        str.append("\n");
+
+        str.append(String.format("%-8s", "s"));
+        for (int i = 0; i < size; i++) {
+            str.append(String.format("%-8s", st[i][4]));
+        }
+
+        return str.toString();
+
+    }
+    
+}
--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -1,8 +1,6 @@
 package grafo;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Stack;
 
 public class Grafo {
 
@@ -11,12 +9,10 @@ public class Grafo {
     public void addVertice(Vertice v) {
 
         if (listaAdj.contains(v)) {
-
             throw new IllegalArgumentException("Verice ja adicionado");
         } else {
             listaAdj.add(v);
         }
-
         listaAdj.sort(null);
 
     }
@@ -53,134 +49,14 @@ public class Grafo {
     }
 
     public String getVetorRotasDijkstra(String id) {
-        this.dijkstra(id);
-        StringBuilder str = new StringBuilder();
-        int size = listaAdj.size();
-        String[][] st = new String[listaAdj.size()][5];
-
-        for (int i = 0; i < listaAdj.size(); i++) {
-            Vertice v = listaAdj.get(i);
-            st[i][0] = v.getId();
-            st[i][1] = Integer.toString(v.getD());
-            st[i][2] = v.getPi();
-            st[i][3] = " ";
-            st[i][4] = "x";
-        }
-
-        // Cabeçalhos
-        str.append(String.format("%-8s", "VERTICE"));
-        for (int i = 0; i < size; i++) {
-            str.append(String.format("%-8s", st[i][0]));
-        }
-        str.append("\n");
-
-        str.append(String.format("%-8s", "d"));
-        for (int i = 0; i < size; i++) {
-            str.append(String.format("%-8s", st[i][1]));
-        }
-        str.append("\n");
-
-        str.append(String.format("%-8s", "Pi"));
-        for (int i = 0; i < size; i++) {
-            str.append(String.format("%-8s", st[i][2]));
-        }
-        str.append("\n");
-
-        str.append(String.format("%-8s", "q"));
-        for (int i = 0; i < size; i++) {
-            str.append(String.format("%-8s", st[i][3]));
-        }
-        str.append("\n");
-
-        str.append(String.format("%-8s", "s"));
-        for (int i = 0; i < size; i++) {
-            str.append(String.format("%-8s", st[i][4]));
-        }
-
-        return str.toString();
+        Dijkstra d = new Dijkstra(listaAdj);
+        return d.vetorRoteamentoToString(id);
     }
 
-    private String[][] dijkstra(String id) {
-        Vertice v = this.getVertice(id);
-        if (v != null) {
-            this.initializaSingleSource(id);
-        } else {
-            return null;
-        }
+    
 
-        ArrayList<Vertice> s = new ArrayList<>();
-        PriorityQueue<Vertice> q = new PriorityQueue<>(new VerticeDijkstraComparator());
-        for (Vertice vertice : listaAdj) {
-            q.add(vertice);
-        }
+   
 
-        while (!q.isEmpty()) {
-            Vertice u = q.poll();
-            s.add(u);
-            ArrayList<Aresta> listaArestas = u.getListaArestas();
-            PriorityQueue<Aresta> filaArestas = new PriorityQueue<>(new ArestaComparator());
-            for (Aresta aresta : listaArestas) {
-                filaArestas.add(aresta);
-            }
-            while (!filaArestas.isEmpty()) {
-                Aresta aresta = filaArestas.poll();
-                Vertice adj = aresta.getVerticeAdj();
-                
-                this.relax(u, adj, aresta.getCusto());
-                if(q.contains(adj)){
-                    q.remove(adj);
-                    q.add(adj);
-                }
-                
-            }
-
-        }
-        return null;
-
-    }
-
-    private void relax(Vertice u, Vertice verticeAdj, int custo) {
-
-        if (verticeAdj.getD() > u.getD() + custo) {
-            verticeAdj.setD(u.getD() + custo);
-            verticeAdj.setPi(u.getId());
-        }
-    }
-
-    private int getPosicaoId(String[][] vet, String id) {
-
-        for (int i = 0; i < vet.length; i++) {
-            if (vet[i][0].equals(id)) {
-                return i;
-            }
-
-        }
-        // nao vai chegar aqui
-        return 0;
-    }
-
-    private void setVetorDijkstra(String[][] vet, String id, String d, String pi) {
-
-        for (int i = 0; i < vet.length; i++) {
-            if (vet[i][0].equals(id)) {
-                vet[i][1] = d;
-                vet[i][2] = pi;
-            }
-        }
-    }
-
-    private void initializaSingleSource(String id) {
-
-        for (Vertice v : listaAdj) {
-            v.setD(Integer.MAX_VALUE);
-            v.setPi("nill");
-
-            if (v.getId().equals(id)) {
-                v.setD(0);
-                v.setPi("nill");
-            }
-        }
-
-    }
+  
 
 }
